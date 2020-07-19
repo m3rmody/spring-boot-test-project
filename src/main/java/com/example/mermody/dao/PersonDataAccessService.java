7244ca94e49afd3bdb7eed82570ded3571d24e93
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-07-19 17:37:59 +0200

    Create updateMethod and Delete

---
+++ src/main/java/com/example/mermody/dao/PersonDataAccessService.java
@@ -0,0 +1,34 @@
+package com.example.mermody.dao;
+
+import com.example.mermody.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PersonDataAccessService implements PersonDao{
+    @Override
+    public int isertperson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/mermody/api/PersonControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.mermody.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonControllerTest {
+
+}
